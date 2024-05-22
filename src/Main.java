d54df7d7478685d11a105f752d4573a6fd05ac1c
Ermmoha <ermoh_00@mail.ru>
2024-05-22 10:26:22 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("needchanged.txt"));
+             FileWriter writer = new FileWriter("changed.txt")){
+            StringBuilder stb = new StringBuilder();
+            String text;
+            Pattern pattern = Pattern.compile("(/\\*([\\S\\s]+?)\\*/)|(//.*)");
+            while ((text = reader.readLine()) != null) {
+                stb.append(text);
+                stb.append("\n");
+            }
+            text = stb.toString();
+            Matcher matcher = pattern.matcher(stb);
+            text = matcher.replaceAll(" ");
+            writer.write(text);
+        } catch (Exception ex) {
+            System.out.println("ОшибкА");
+        }
+    }
+}
